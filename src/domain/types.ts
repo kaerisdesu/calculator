fd6569889204b569dd88b3e44f98cfd6fea32814
kaerisdesu <kaeris.desu@yandex.ru>
2026-05-11 20:41:40 +0300

    added types and initial state

---
+++ src/domain/types.ts
@@ -0,0 +1,17 @@
+export type Operator = "+" | "-" | "*" | "/";
+
+export type CalculatorState = Readonly<{
+  display: string;
+  previous: number | null;
+  operation: Operator | null;
+  awaitingOperand: boolean;
+  error: string | null;
+}>;
+
+export const INITIAL_STATE: CalculatorState = Object.freeze({
+  display: "0",
+  previous: null,
+  operation: null,
+  awaitingOperand: false,
+  error: null,
+});
